4f68e4e7420feb6799aca0715047589667ed7414
Brittany Petersen <britt.petersen100@gmail.com>
2022-02-17 22:03:09 -0700

    added resource router and model requests

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,25 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig');
+
+function findResource() {
+    return db('resource')
+}
+
+function findById(resource_id) {
+    return db('resource')
+        .where({ resource_id: Number(resource_id)})
+        .first()
+}
+
+function addResource(resource) {
+    return db('resource')
+    .insert(resource)
+    .then(resource_id => {
+        return findById(resource_id[0])
+    })
+}
+
+module.exports = {
+    findResource,
+    findById,
+    addResource
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,4 +1,21 @@
 const router = require('express').Router();
+const Resource = require('./model');
 
+router.get('/', (req, res, next) => {
+    Resource.findResource()
+    .then(resources => {
+        res.status(200).json(resources)
+    })
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    const data = req.body;
+    Resource.addResource(data)
+        .then(data => {
+            res.status(201).json(data)
+        })
+        .catch(next)
+})
 
 module.exports = router;
