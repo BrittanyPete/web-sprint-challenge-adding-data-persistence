2cbb8c88155d898b01edf63f91eb162ba6df827f
Brittany Petersen <britt.petersen100@gmail.com>
2022-02-18 00:52:41 -0700

    added requests for task router and model, all tests passing

--- api/task/model.js
+++ api/task/model.js
@@ -1,5 +1,22 @@
 const db = require('../../data/dbConfig');
 
-function findTask() {
+async function findTask() {
+    return db('tasks as t')
+        .leftJoin('projects as p', 'p.project_id', 't.project_id' )
+        .select('t.task_id', 't.task_description', 't.task_notes', 't.task_completed', 'p.project_name', 'p.project_description')
+        .orderBy('t.task_id')
+}
+
+function getTasks() {
     return db('tasks')
+}
+
+async function addTask(task) {
+    const [task_id] = await db('tasks').insert(task);
+    return getTasks().where({task_id}).first()
+}
+
+module.exports = {
+    findTask,
+    addTask
 }
--- api/task/router.js
+++ api/task/router.js
@@ -1,5 +1,45 @@
 const router = require('express').Router();
+const Tasks = require('./model');
 
+router.get('/', (req, res, next) => {
+    const editedTask = [];
+    Tasks.findTask()
+        .then(tasks => {
+            tasks.forEach(task => {
+                if(task.task_completed === 0) {
+                    editedTask.push({
+                        ...task,
+                        task_completed: false
+                    })
+                } else {
+                    editedTask.push({
+                        ...task,
+                        task_completed: true
+                    })
+                }
+            })
+            res.status(200).json(editedTask);
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Tasks.addTask(req.body)
+        .then(newTask => {
+            if(newTask.task_completed === 0) {
+                res.status(201).json({
+                    ...newTask,
+                    task_completed: false
+                })
+            } else {
+                res.status(201).json({
+                    ...newTask,
+                    task_completed: true
+                })
+            }
+        })
+        .catch(next)
+})
 
 module.exports = router;
 
