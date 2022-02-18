bf00e43316a9d8a1dad7ce360fee80fe1f3a3992
Brittany Petersen <britt.petersen100@gmail.com>
2022-02-17 22:15:44 -0700

    added requests for project router and model

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,24 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+function findProject() {
+    return db('projects')
+}
+
+function findById(project_id) {
+    return db('projects')
+        .where({ project_id: Number(project_id)})
+        .first()
+}
+
+function addProject(project) {
+    return db('projects')
+    .insert(project)
+    .then(project_id => {
+        return findById(project_id[0])
+    })
+}
+
+module.exports = {
+    findProject,
+    addProject
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,4 +1,21 @@
 const router = require('express').Router();
+const Project = require('./model');
 
+router.get('/', (req, res, next) => {
+    Project.findProject()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    const projData = req.body;
+    Project.addProject(projData)
+        .then(data => {
+            res.status(201).json(data)
+        })
+        .catch(next)
+})
 
 module.exports = router;
